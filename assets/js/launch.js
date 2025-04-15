a5c86ed57aef800475c665e5852c31121ab22b94
lucas-lob <lucas.mcp23@gmail.com>
2025-04-15 12:41:19 -0300

    :heavy_plus_sign: feat: creates object with launch datas

---
+++ assets/js/launch.js
@@ -0,0 +1,63 @@
+const launch = {
+    desktopLaunch: [
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        },
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        },
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        },
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        },
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        }
+    ],
+    mobileLaunch: [
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        },
+        {
+            imgSrc: "../images/body/launch.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: 100.00,
+            descountedPrice: 79.90,
+            off: 10,
+            maxInstallments: 10
+        },
+    ]
+}
+
