303a6d1cf9d8df048d50412fa6657e8097cdd68a
lucas-lob <lucas.mcp23@gmail.com>
2025-04-20 21:27:48 -0300

    :heavy_plus_sign: feat: adds top search bar functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,27 @@
 // Global variables
-const searchInputResult = document.getElementById()
+const searchInputBarMobile = document.getElementById("mobile-top-search-bar")
+const searchInputButtonMobile = document.getElementById("mobile-top-search-bar-button")
+const searchInputResultMobile = document.getElementById("mobile-top-search-bar-result")
+
+const searchInputBarDesktop = document.getElementById("desktop-top-search-bar")
+const searchInputButtonDesktop = document.getElementById("desktop-top-search-bar-button")
+const searchInputResultDesktop = document.getElementById("desktop-top-search-bar-result")
+
+// === MOBILE ==== //
+// Search bar buttons functions
+searchInputButtonMobile.addEventListener("click", () => {
+    if (searchInputBarMobile.value !== "") {
+        searchInputResultMobile.innerHTML = searchInputBarMobile.value
+    }
+})
+
+
+
+// === DESKTOP === //
+// Search bar buttons functions
+searchInputButtonDesktop.addEventListener("click", () => {
+    if (searchInputBarDesktop.value !== "") {
+        searchInputResultDesktop.innerHTML = searchInputBarDesktop.value
+    }
+})
+
