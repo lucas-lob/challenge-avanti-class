15ed878a45222e598de398b794a8ae4e56da8f6b
lucas-lob <lucas.mcp23@gmail.com>
2025-04-22 16:40:33 -0300

    :hammer: feat: adds funcionality to the top bar search result

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,12 +7,22 @@ const searchInputBarDesktop = document.getElementById("desktop-top-search-bar")
 const searchInputButtonDesktop = document.getElementById("desktop-top-search-bar-button")
 const searchInputResultDesktop = document.getElementById("desktop-top-search-bar-result")
 
+const searchInputContainer = document.getElementsByClassName("top-search-bar-result")
+const searchInputCloseButton = document.getElementsByClassName("top-search-bar-close-button")
+
+
 // === MOBILE ==== //
 // Search bar buttons functions
 searchInputButtonMobile.addEventListener("click", () => {
     if (searchInputBarMobile.value !== "") {
         searchInputResultMobile.innerHTML = searchInputBarMobile.value
+        searchInputBarMobile.value = ""
+        searchInputContainer[1].classList.remove("none")
     }
+})
+
+searchInputCloseButton[1].addEventListener("click", () => {
+    searchInputContainer[1].classList.add("none")
 })
 
 
@@ -22,6 +32,12 @@ searchInputButtonMobile.addEventListener("click", () => {
 searchInputButtonDesktop.addEventListener("click", () => {
     if (searchInputBarDesktop.value !== "") {
         searchInputResultDesktop.innerHTML = searchInputBarDesktop.value
+        searchInputBarDesktop.value = ""
+        searchInputContainer[0].classList.remove("none")
     }
+})
+
+searchInputCloseButton[0].addEventListener("click", () => {
+    searchInputContainer[0].classList.add("none")
 })
 
