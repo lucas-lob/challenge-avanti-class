042d057e7181757e2a6923155fbdc934b7627cab
lucas-lob <lucas.mcp23@gmail.com>
2025-04-23 09:05:27 -0300

    :hammer: feat: adds event listener to update anouncemment slider bottom circles

--- assets/js/anouncemmnet-slider.js
+++ assets/js/anouncemmnet-slider.js
@@ -1,23 +1,44 @@
 {
-
     sliderSettings = {
+        sliderID: "anouncemment-slider",
         bottomCirclesID: "anouncemment-slider-bottom-circles"
     }
 
 
     // Global variables
-    bottomCirclesDiv = document.getElementById(sliderSettings.bottomCirclesID)
+    const sliderDiv = document.getElementById(sliderSettings.sliderID)
+    const bottomCirclesDiv = document.getElementById(sliderSettings.bottomCirclesID)
+
+
 
     // Creates bottom circles
     bottomCirclesDiv.innerHTML =
         `
-                    <span class="material-icons anouncemment-slider-bottom-selected-circle">circle</span>
+                    <span class="material-icons anouncemment-slide-selected">circle</span>
                     <span class="material-icons">circle</span>
                     <span class="material-icons">circle</span>
 
         `
-    let bottomCircles = document.querySelector(`div.${sliderSettings.bottomCirclesClass}`)
 
 
+    // Adds event listener for update the bottom circles
+    sliderDiv.addEventListener("touchend", () => {
+        let screenSize = window.innerWidth
 
+        setTimeout(() => {
+            for (let i = 0; i < sliderDiv.childElementCount; i++) {
+                let slidePosition = sliderDiv.children[i].getBoundingClientRect().x
+
+                if (slidePosition >= -100 && slidePosition <= 200) {
+                    for(let k = 0; k < bottomCirclesDiv.childElementCount; k++){
+                        if(k === i){
+                            bottomCirclesDiv.children[k].classList.add("anouncemment-slide-selected")
+                        } else {
+                            bottomCirclesDiv.children[k].classList.remove("anouncemment-slide-selected")
+                        }
+                    }
+                }
+            }
+        }, 500)
+    })
 }
